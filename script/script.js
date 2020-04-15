a316e55773c3827a545bd04c5ca4f7a1a5fa5040
Max Nikitin <maxxnikitin90@yandex.ru>
2020-04-15 14:33:32 +0300

    fixed js

--- script/script.js
+++ script/script.js
@@ -4,25 +4,25 @@ const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
 const nameInput = document.querySelector('#fullname');
 const jobInput = document.querySelector('#about');
-
-function addPopup () {
-  popup.classList.add('popup_opened');
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileAbout.textContent;
-}
-buttonEdit.addEventListener('click', addPopup);
-
 const buttonClose = document.querySelector('.button_close-item');
-function removePopup () {
-  popup.classList.remove('popup_opened');
-}
-buttonClose.addEventListener('click', removePopup);
-
 const formElement = document.querySelector('.form');
+
+function changePopup () {
+  if (popup.classList.contains('popup_opened')) {
+    popup.classList.remove('popup_opened');
+  } else {
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileAbout.textContent;
+  }
+}
+
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileAbout.textContent = jobInput.value;
-    removePopup();
+    changePopup();
 }
 formElement.addEventListener('submit', formSubmitHandler);
+buttonEdit.addEventListener('click', changePopup);
+buttonClose.addEventListener('click', changePopup);
