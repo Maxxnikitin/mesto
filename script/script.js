9490293cbf5b4131fc336f90059c5434d2d164d1
Max Nikitin <maxxnikitin90@yandex.ru>
2020-04-24 18:52:25 +0300

    add new popup to add images

--- script/script.js
+++ script/script.js
@@ -1,28 +1,43 @@
 const buttonEdit = document.querySelector('.button_edit');
-const popup = document.querySelector('.popup');
+const buttonAdd = document.querySelector('.button_add');
+const popupProfile = document.querySelector('#popup__profile');
+const popupNewImages = document.querySelector('#popup__new-images');
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
 const nameInput = document.querySelector('#fullname');
 const jobInput = document.querySelector('#about');
-const buttonClose = document.querySelector('.button_close-item');
+const titleInput = document.querySelector('#place-title');
+const linkInput = document.querySelector('#image-link');
+const buttonCloseProfile = document.querySelector('#close__profile');
+const buttonCloseNewImages = document.querySelector('#close__new-images');
 const formElement = document.querySelector('.form');
 
-function changePopup () {
-  if (popup.classList.contains('popup_opened')) {
-    popup.classList.remove('popup_opened');
+//Отвечает за открытие/закрытие попапа с редактированием профиля
+function changePopupProfile () {
+  if (popupProfile.classList.contains('popup_opened')) {
+    popupProfile.classList.remove('popup_opened');
   } else {
-    popup.classList.add('popup_opened');
+    popupProfile.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileAbout.textContent;
   }
+}
+
+//Отвечает за открытие/закрытие попапа с добавлением фото
+function changePopupNewImages () {
+  popupNewImages.classList.toggle('popup_opened');
+  titleInput.value = '';
+  linkInput.value = '';
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileAbout.textContent = jobInput.value;
-    changePopup();
+    changePopupProfile();
 }
 formElement.addEventListener('submit', formSubmitHandler);
-buttonEdit.addEventListener('click', changePopup);
-buttonClose.addEventListener('click', changePopup);
+buttonEdit.addEventListener('click', changePopupProfile);
+buttonAdd.addEventListener('click', changePopupNewImages);
+buttonCloseProfile.addEventListener('click', changePopupProfile);
+buttonCloseNewImages.addEventListener('click', changePopupNewImages);
