9261d0ddff18ece1ba0f842bda139d77eebae845
Max Nikitin <maxxnikitin90@yandex.ru>
2020-07-01 00:31:00 +0300

    fix bugs

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,33 +1,33 @@
 import {Popup} from './Popup.js';
 
 export class PopupWithForm extends Popup {
-    constructor(popupSelector, {submitForm}) {
-        super(popupSelector);
-        this.submitForm = submitForm;
-    }
+  constructor(popupSelector, {submitForm}) {
+    super(popupSelector);
+    this.submitForm = submitForm;
+  }
 
-    _setSubmitForm(evt) {
-        evt.preventDefault();
-        this.submitForm(this._getInputValues());
-    }
+  _setSubmitForm(evt) {
+    evt.preventDefault();
+    this.submitForm(this._getInputValues());
+  }
 
-    _getInputValues() {
-        const item = {
-            name: this.popupSelector.querySelector('.form__input_name').value,
-            link: this.popupSelector.querySelector('.form__input_link').value
-        };
-        return item;
-    }
+  _getInputValues() {
+    const item = {
+      name: this.popupSelector.querySelector('.form__input_name').value,
+      link: this.popupSelector.querySelector('.form__input_link').value
+    };
+    return item;
+  }
 
-    close() {
-        this.popupSelector.removeEventListener('submit', this.submitForm);
-        this.popupSelector.querySelector('form').reset();
-        super.close();
-    }
+  close() {
+    this.popupSelector.removeEventListener('submit', this.submitForm);
+    this.popupSelector.querySelector('form').reset();
+    super.close();
+  }
 
-    setEventListeners() {
-      this._submit = this._setSubmitForm.bind(this);
-      this.popupSelector.addEventListener('submit', this._submit, {once: true});
-      super.setEventListeners();
+  setEventListeners() {
+    this._submit = this._setSubmitForm.bind(this);
+    this.popupSelector.addEventListener('submit', this._submit, {once: true});
+    super.setEventListeners();
   }
 }
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,19 +1,19 @@
 export class UserInfo {
   constructor(author) {
-      this._name = author.profileAuthor;
-      this._info = author.profileStatus;
+    this._name = author.profileAuthor;
+    this._info = author.profileStatus;
   }
 
   getUserInfo() {
-      const userInfo = {
-          name: this._name.textContent,
-          info: this._info.textContent
-      }
-      return userInfo;
+    const userInfo = {
+      name: this._name.textContent,
+      info: this._info.textContent
+    }
+    return userInfo;
   }
 
   setUserInfo(data) {
-      this._name.textContent = data.name;
-      this._info.textContent = data.link;
+    this._name.textContent = data.name;
+    this._info.textContent = data.link;
   }
 }
